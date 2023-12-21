0e47331fb99f34826be0c4b812fc13da225d0338
a-dugonin <a_dugonin@mail.ru>
2023-12-21 20:31:48 +0300

    task_2: Добавление строк кода в файл main.py для запуска скрипта

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from taxes import Apartment, Car, CountryHouse
+from karma import Buddhist
 
-count_money = float(input('Введите количество ваших денег: '))
-apartment = Apartment(float(input('Введите стоимость квартиры: ')))
-print(apartment)
-car = Car(float(input('Введите стоимость авто: ')))
-print(car)
-house = CountryHouse(float(input('Введите стоимость дачи: ')))
-print(house)
-tax_amount = apartment.calculate_tax() + car.calculate_tax() + house.calculate_tax()
-if count_money < tax_amount:
-    print('Пришло время заплатить по счетам!')
-    print(
-        f'Ты должен в казну {tax_amount}, но у тебя {count_money}! Прийдется напрячься, чтобы заплатить по счетам!'
-    )
-else:
-    print(f'Ты можешь спать спокойно! Налоги уплачены!')
+# count_money = float(input('Введите количество ваших денег: '))
+# apartment = Apartment(float(input('Введите стоимость квартиры: ')))
+# print(apartment)
+# car = Car(float(input('Введите стоимость авто: ')))
+# print(car)
+# house = CountryHouse(float(input('Введите стоимость дачи: ')))
+# print(house)
+# tax_amount = apartment.calculate_tax() + car.calculate_tax() + house.calculate_tax()
+# if count_money < tax_amount:
+#     print('Пришло время заплатить по счетам!')
+#     print(
+#         f'Ты должен в казну {tax_amount}, но у тебя {count_money}! Прийдется напрячься, чтобы заплатить по счетам!'
+#     )
+# else:
+#     print(f'Ты можешь спать спокойно! Налоги уплачены!')
+
+buddha = Buddhist()
+buddha.life()
