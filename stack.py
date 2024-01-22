8594b0b09661e515bfc8ffcbc318dcc11ea86b37
a-dugonin <a_dugonin@mail.ru>
2024-01-22 18:23:20 +0300

    task_5: Добавление аннотаций и описания методов и классов в файле stack.py

--- stack.py
+++ stack.py
@@ -1,21 +1,57 @@
 class Stack(list):
-    def append(self, elem, index: int = 0):
-        self.insert(index - 1, elem)
+    """
+    Дочерний класс Stack, родитель: list
+    Позволяет создать стек - абстрактный тип данных, представляющий собой список элементов, организованных по принципу
+    LIFO (англ. last in — first out, «последним пришёл — первым вышел»).
+    Данный класс унаследован от класса list, поэтому имеет все его родительские методы.
+    В данном классе переопределен только метод append родительского класса list
+    """
+
+    def append(self, elem, index: int = 0) -> None:
+        """
+        Метод добавляет элементы относительно переданного индекса. Если индекс не передан в метод, то элемент
+        по умолчанию становится на первое место в списке
+        :param elem: object
+        :param index: int
+        :return: None
+        """
+        if index >= len(self):
+            self.insert(index - 1, elem)
+        else:
+            self.insert(index, elem)
 
 
 class TaskManager:
-    def __init__(self):
+    """
+    Базовый класс TaskManager позволяющий создать менеджер задач.
+    Аттрибуты:
+        task_list - стек задач, по умолчанию пустой
+        task_priorities_available - стек имеющихся приоритетов задач, по умолчанию пустой
+    """
+    def __init__(self) -> None:
         self.task_list = Stack()
         self.task_priorities_available = Stack()
 
     @staticmethod
-    def create_new_task(task, priority):
+    def create_new_task(task: str, priority: int) -> Stack:
+        """
+        Метод позволяет создать новый список задач с определенным приоритетом.
+        :param task: задача
+        :param priority: приоритет задачи
+        :return: Stack
+        """
         new_task = Stack()
         new_task.append(priority)
         new_task.append(task)
         return new_task
 
-    def add_task(self, task, priority):
+    def add_task(self, task: str, priority: int) -> None:
+        """
+        Метод добавляет задачу в менеджер задач согласно приоритета задачи
+        :param task: задача
+        :param priority: приоритет задачи
+        :return: None
+        """
         if priority not in self.task_priorities_available:
             self.task_list.append(self.create_new_task(task, priority), priority)
             self.task_priorities_available.append(priority, priority)
@@ -27,7 +63,12 @@ class TaskManager:
                 if task not in self.task_list[priority - 1]:
                     self.task_list[priority - 1].append(task)
 
-    def remove_task(self, task):
+    def remove_task(self, task: str) -> None:
+        """
+        Метод удаляет задачу из менеджера задач
+        :param task: задача
+        :return: None
+        """
         for tasks in self.task_list:
             if task in tasks and len(tasks) > 2:
                 tasks.remove(task)
@@ -35,7 +76,7 @@ class TaskManager:
                 if task in tasks:
                     self.task_list.remove(tasks)
 
-    def __str__(self):
+    def __str__(self) -> str:
         task_list_string = 'Список задач:\n'
         for task in self.task_list:
             task_list_string += f"{task[-1]} - {'; '.join(task[:-1])}\n"
@@ -52,6 +93,7 @@ if __name__ == '__main__':
     manager.add_task("сдать ДЗ", 2)
     manager.add_task("погладить кошку", 5)
     manager.add_task("выпить кофе", 3)
+    manager.add_task('почистить кофемашину', 1)
     print(manager)
     print()
     manager.remove_task('поесть')
