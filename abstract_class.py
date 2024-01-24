17b6ce160b2b2e2962726a283030a2a5a4b46052
a-dugonin <a_dugonin@mail.ru>
2024-01-24 15:59:15 +0300

    task_6: Реализован основной функционал абстрактого класса и дочерних классов унаследованных от родительского абстрактного класса

--- abstract_class.py
+++ abstract_class.py
@@ -3,37 +3,84 @@ from abc import ABC, abstractmethod
 
 
 class Shape(ABC):
-
+    """
+    Базовый абстрактный класс для создания фигур. Имеет абстрактный метод area вычисляющий площадь фигуры
+    """
     @abstractmethod
-    def area(self):
+    def area(self) -> None:
+        """
+        Абстрактный метод, позволяющий вычислять площадь фигуры. Требует переопределения в дочерних классах.
+        :return: None
+        """
         pass
 
 
 class Circle(Shape):
-    def __init__(self, radius):
+    """
+    Дочерний класс Circle. Родитель Shape. Позволяет создать фигуру круг
+    Аргументы:
+        radius (int | float) - передается радиус круга
+    """
+    def __init__(self, radius: int | float) -> None:
         self.radius = radius
 
     @property
-    def area(self):
+    def area(self) -> int | float:
+        """
+        Метод позволяет вычислить площадь круга
+        :return: int | float
+        """
         return math.pi * self.radius**2
 
 
 class Rectangle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
+    """
+    Дочерний класс Rectangle. Родитель Shape. Позволяет создать фигуру прямоугольник
+    Аргументы:
+        side_a (int | float) - длина стороны a
+        side_b (int | float) - длина стороны b
+    """
+    def __init__(self, side_a: int | float, side_b: int | float) -> None:
+        self.side_a = side_a
+        self.side_b = side_b
 
-    def area(self):
-        return math.pi * self.radius**2
+    @property
+    def area(self) -> int | float:
+        """
+        Метод позволяет вычислить площадь прямоугольника
+        :return: int | float
+        """
+        return self.side_a * self.side_b
 
 
 class Triangle(Shape):
-    def __init__(self, basis, height):
+    """
+    Дочерний класс Triangle. Родитель Shape. Позволяет создать фигуру треугольник
+    Аргументы:
+        basis (int | float) - длина основания треугольника
+        height (int | float) - длина стороны треугольника
+    """
+    def __init__(self, basis: int | float, height: int | float) -> None:
         self.basis = basis
         self.height = height
 
-    def area(self):
+    @property
+    def area(self) -> int | float:
+        """
+        Метод позволяет вычислить площадь треугольника
+        :return: int | float
+        """
         return 0.5 * self.basis * self.height
 
 
-circle = Circle(5)
-print(circle.area)
+def main():
+    circle = Circle(5)
+    triangle = Triangle(3, 5)
+    rectangle = Rectangle(4, 8)
+    print(circle.area)
+    print(triangle.area)
+    print(rectangle.area)
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,8 @@
+from abstract_class import Circle, Triangle, Rectangle
+
+circle = Circle(5)
+triangle = Triangle(3, 5)
+rectangle = Rectangle(4, 8)
+print(circle.area)
+print(triangle.area)
+print(rectangle.area)
